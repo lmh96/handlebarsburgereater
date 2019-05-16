04d4fa41a11aee75584e13b48e5f5729baec60bd
logan hill <loganhill@logans-MacBook-Pro.local>
2019-05-15 22:51:06 -0400

    added jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
 const mysql = require("mysql");
 
-const conn = mysql.createConnection({
-    host: "ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
-    port: 3306,
-    user: "hno4jieu42tos1ml	",
-    password: "qo5mlsfb2mb7jvqa",
-    database: "i3ru4wljmfaw3psd",
-});
+// const conn = mysql.createConnection({
+//     host: "ctgplw90pifdso61.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+//     port: 3306,
+//     user: "hno4jieu42tos1ml	",
+//     password: "qo5mlsfb2mb7jvqa",
+//     database: "i3ru4wljmfaw3psd",
+// });
+
+const conn = mysql.createConnection(process.env.JAWSDB_URL)
 
 conn.connect(function(err) {
     if(err) throw err;
